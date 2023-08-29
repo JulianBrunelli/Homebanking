59407e1fcfb248e1bfc5feb9f45e7ea1d105a3ba
JulianBrunelli <Julianbrunelli@outlook.com>
2023-08-29 17:41:50 -0300

    TransactionController created

---
+++ src/main/java/com/mindhub/homebanking/controllers/TransactionController.java
@@ -0,0 +1,76 @@
+package com.mindhub.homebanking.controllers;
+
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.models.Transaction;
+import com.mindhub.homebanking.models.TransactionType;
+import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import com.mindhub.homebanking.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api")
+public class TransactionController {
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Transactional
+    @PostMapping("/transactions")
+    public ResponseEntity<Object> createTransaction(
+            Authentication authentication, @RequestParam double amount, @RequestParam String description,
+            @RequestParam String originAccountNumber, @RequestParam String destinationAccountNumber){
+
+        Client client = clientRepository.findByEmail(authentication.getName());
+        Account originAccount = client.getAccounts().stream().filter(account -> account.getNumber().equals(originAccountNumber)).findFirst().orElse(null);
+        Account destinationAccount = accountRepository.findByNumber(destinationAccountNumber);
+
+
+        if (originAccount.getNumber() == destinationAccount.getNumber()){
+            return new ResponseEntity<>("Source and destination accounts can't be the same", HttpStatus.FORBIDDEN);
+        }
+        if(originAccount == null){
+            return new ResponseEntity<>("Origin account not found", HttpStatus.FORBIDDEN);
+        }
+        if(destinationAccount == null){
+            return new ResponseEntity<>("Destination account not found", HttpStatus.FORBIDDEN);
+        }
+        if(amount <= 0){
+            return new ResponseEntity<>("Amount must be greater than 0", HttpStatus.FORBIDDEN);
+        }
+        if(description.isBlank()){
+            return new ResponseEntity<>("Submit a description", HttpStatus.FORBIDDEN);
+        }
+        if (client == null){
+            return new ResponseEntity<>("Client not found", HttpStatus.FORBIDDEN);
+        }
+        if (originAccount.getBalance() < amount){
+            return new ResponseEntity<>("Not enough money in the account", HttpStatus.FORBIDDEN);
+        } else {
+            originAccount.setBalance(originAccount.getBalance() - amount);
+            destinationAccount.setBalance(destinationAccount.getBalance() + amount);
+            Transaction transactionDebit = new Transaction(amount, description, LocalDateTime.now(), TransactionType.DEBIT);
+            Transaction transactionCredit = new Transaction(amount, description, LocalDateTime.now(), TransactionType.CREDIT);
+            originAccount.addTransaction(transactionDebit);
+            destinationAccount.addTransaction(transactionCredit);
+            transactionRepository.save(transactionDebit);
+            transactionRepository.save(transactionCredit);
+            return new ResponseEntity<>("Transaction created", HttpStatus.CREATED);
+        }
+    }
+}
